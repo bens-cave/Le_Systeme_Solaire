8b1130671bf3464f0f07e446625e2bc400dd9356
bens-cave <103117819+bens-cave@users.noreply.github.com>
2022-12-08 16:36:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <main className='site-wrapper'>
-      <BrowserRouter>
+      <BrowserRouter basename="/le_systeme_solaire" >
         <PageNavBar />
         <Routes>
           <Route path="/" element={<Sun />} />
